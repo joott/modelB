07bac87d784e6e10920654b5d0d9e872b1312c48
Josh Ott <joshott16@gmail.com>
2022-05-18 17:58:23 -0400

    added rest of model B implementation

--- modelB.jl
+++ modelB.jl
@@ -1,51 +1,85 @@
+cd(@__DIR__)
+
 using Plots
 using LaTeXStrings
+using Distributions
 
-L = 8
-v = [0 0 0; 1 0 0; 0 1 0; 1 1 0]
-colors = [:black, :red, :green, :blue]
+const L = 8 # must be a multiple of 4
+const λ = 4.0f0
+const Γ = 1.0f0
+const T = 1.0f0
 
-function update(ϕ, x1, x2, m)
-    if x1[3]==L
-        plot!([x1[3], L+1].+0.5, [x1[1], x2[1]].+0.5, lc=colors[m])
-        plot!([x2[3], 0].+0.5, [x1[1], x2[1]].+0.5, lc=colors[m])
-    else
-        plot!([x1[3], x2[3]].+0.5, [x1[1], x2[1]].+0.5, lc=colors[m])
+const Δt = 0.04f0/Γ
+const Rate = Float32(sqrt(2.0*Δt*Γ))
+
+const v = [0 0 0; 1 0 0; 0 1 0; 1 1 0]
+const μ = [1 0 0; 0 1 0; 0 0 1]
+
+function hotstart(n)
+    rand(Normal(), n, n, n)
+end
+
+function ΔH(x, ϕ, q, m²)
+    @inbounds ϕold = ϕ[x[1], x[2], x[3]]
+    ϕt = ϕold + q
+    Δϕ = ϕt - ϕold
+    Δϕ² = ϕt^2 - ϕold^2
+
+    @inbounds ∑nn = ϕ[x[1]%L+1, x[2], x[3]] + ϕ[x[1], x[2]%L+1, x[3]] + ϕ[x[1], x[2], x[3]%L+1]
+    @inbounds ∑nn += ϕ[(x[1]+L-2)%L+1, x[2], x[3]] + ϕ[x[1], (x[2]+L-2)%L+1, x[3]] + ϕ[x[1], x[2], (x[3]+L-2)%L+1]
+
+    3Δϕ² - Δϕ * ∑nn + 0.5m² * Δϕ² + 0.25λ * (ϕt^4 - ϕold^4)
+end
+
+function step(m², ϕ, x1, x2)
+    q = Rate*rand(Normal())
+
+    @inbounds ϕ1 = ϕ[x1[1], x1[2], x1[3]]
+    @inbounds ϕ2 = ϕ[x2[1], x2[2], x2[3]]
+
+    δH = ΔH(x1, ϕ, q, m²) + ΔH(x2, ϕ, -q, m²) + q^2
+    P = min(1.0f0, exp(-δH))
+    r = rand(Float32)
+    if (r < P)
+        @inbounds ϕ[x1[1], x1[2], x1[3]] += q
+        @inbounds ϕ[x2[1], x2[2], x2[3]] -= q
     end
 end
 
-function sweep(ϕ)
-    plots = []
-    T(i,j,k) = [4(i-1)+2(j-1), j+k-2, k-1]
-    μ = [1 0 0; 0 1 0; 0 0 1]
-    for n in 2:2
-        for m in 1:4
-            for k in 1:L   
-                p = plot()
-                for j in 1:L
-                    for i in 1:L÷4
-                        x0 = T(i,j,k)
-                        idx = ((3-n)%3+1, (4-n)%3+1, (5-n)%3+1)
-                        v0 = v[m,:]
-                        x1 = [x0[idx[1]], x0[idx[2]], x0[idx[3]]] + [v0[idx[1]], v0[idx[2]], v0[idx[3]]]
-                        x2 = x1 + μ[n+1,:]
-                        update(ϕ, x1.%L .+1, x2.%L .+1, m)
-                    end
-                end
-                push!(plots, p)
+function sweep(m², ϕ)
+    transition(i,j,k) = [4(i-1)+2(j-1), j+k-2, k-1]
+    for n in 0:2, m in 1:4
+        Threads.@threads for k in 1:L   
+            for i in 1:L÷4, j in 1:L
+                idx = [(3-n)%3+1, (4-n)%3+1, (5-n)%3+1]
+                x1 = transition(i,j,k)[idx] + v[m,idx]
+                x2 = x1 + μ[n+1,:]
+                step(m², ϕ, x1.%L.+1, x2.%L.+1)
             end
         end
     end
-    plots
 end
 
-ϕ = zeros(Float64, L, L, L)
-result = sweep(ϕ)
-anim = @animate for n in 1:length(result)
-    plot(result[n], legend=false)
-    title!(latexstring("y=", (n-1)%L+1))
-    xaxis!("z", (1,L+1))
-    yaxis!("x", (1,L+1))
+function thermalize(m², ϕ, N=10000)
+    for i in 1:N
+        sweep(m², ϕ)
+    end
 end
 
-gif(anim, "sweeps3.gif", fps=1.5)
+M(ϕ) = sum(ϕ)/L^3
+
+ϕ = hotstart(L)
+m² = -2.285
+
+thermalize(m², ϕ, 100*L^2)
+
+maxt = L^2*500
+
+open("output_$L.dat","w") do io 
+	for i in 0:maxt
+		Mt = M(ϕ)
+		@inbounds ϕk0 = fft(ϕ)[1,1,1] 
+		Printf.@printf(io, "%i %f %f %f\n", i, Mt, real(ϕk0), imag(ϕk0))
+		thermalize(m², ϕ, 100)
+	end
+end
