03b60afb0f9acaca65d252193dacedcb31b43f09
Josh Ott <joshott16@gmail.com>
2022-06-18 15:57:54 -0400

    CUDA optimizations

--- modelB_CUDA.jl
+++ modelB_CUDA.jl
@@ -5,7 +5,6 @@ using Printf
 using FFTW
 using JLD2
 using Random
-using LinearAlgebra
 using CUDA
 using BenchmarkTools
 
@@ -35,8 +34,9 @@ function ΔH(x, ϕ, q, m², L)
 	return 3Δϕ² - Δϕ * ∑nn + 0.5m² * Δϕ² + 0.25λ * (ϕt^4 - ϕold^4)
 end
 
-function step(m², ϕ, x1, x2, L, R, r_i)
-	q = Rate*R[r_i]
+function step(m², ϕ, x1, x2, L)
+	norm = cos(2π*rand())*sqrt(-2*log(rand()))
+	q = Rate*norm
 
 	δH = ΔH(x1, ϕ, q, m², L) + ΔH(x2, ϕ, -q, m², L) + q^2
 	P = min(1.0f0, exp(-δH))
@@ -46,7 +46,7 @@ function step(m², ϕ, x1, x2, L, R, r_i)
 	@inbounds ϕ[x2...] -= q * (r<P)
 end
 
-function sweep(m², ϕ, R, threads, blocks)
+function sweep(m², ϕ, threads, blocks)
 	#=
 	n=0 : (i,j,k)->(x,y,z)
 	n=1 : (i,j,k)->(y,z,x)
@@ -54,13 +54,13 @@ function sweep(m², ϕ, R, threads, blocks)
 	pairs are in i direction
 	=#
 	for m in 1:4
-		kernel_i(m², ϕ, L, m, R; threads, blocks)
-		kernel_j(m², ϕ, L, m, R; threads, blocks)
-		kernel_k(m², ϕ, L, m, R; threads, blocks)
+		kernel_i(m², ϕ, L, m; threads, blocks)
+		kernel_j(m², ϕ, L, m; threads, blocks)
+		kernel_k(m², ϕ, L, m; threads, blocks)
 	end
 end
 
-function gpu_sweep_i(m², ϕ, L, m, R)
+function gpu_sweep_i(m², ϕ, L, m)
     index = (blockIdx().x - 1) * blockDim().x + threadIdx().x - 1
     stride = gridDim().x * blockDim().x
 
@@ -72,13 +72,12 @@ function gpu_sweep_i(m², ϕ, L, m, R)
         x1 = ((4i + 2j + m%2)%L+1, (j + k + m÷2)%L+1, k%L+1)
         @inbounds x2 = (x1[1]%L+1, x1[2], x1[3])
 
-		r_i = L^3÷4 * 3(m-1) + l + 1
-		step(m², ϕ, x1, x2, L, R, r_i)
+		step(m², ϕ, x1, x2, L)
     end
     return
 end
 
-function gpu_sweep_j(m², ϕ, L, m, R)
+function gpu_sweep_j(m², ϕ, L, m)
     index = (blockIdx().x - 1) * blockDim().x + threadIdx().x - 1
     stride = gridDim().x * blockDim().x
 
@@ -90,13 +89,12 @@ function gpu_sweep_j(m², ϕ, L, m, R)
         x1 = (k%L+1, (4i + 2j + m%2)%L+1, (j + k + m÷2)%L+1)
         @inbounds x2 = (x1[1], x1[2]%L+1, x1[3])
 
-		r_i = L^3÷4 * (1 + 3(m-1)) + l + 1
-		step(m², ϕ, x1, x2, L, R, r_i)
+		step(m², ϕ, x1, x2, L)
     end
     return
 end
 
-function gpu_sweep_k(m², ϕ, L, m, R)
+function gpu_sweep_k(m², ϕ, L, m)
     index = (blockIdx().x - 1) * blockDim().x + threadIdx().x - 1
     stride = gridDim().x * blockDim().x
 
@@ -108,16 +106,14 @@ function gpu_sweep_k(m², ϕ, L, m, R)
         x1 = ((j + k + m÷2)%L+1, k%L+1, (4i + 2j + m%2)%L+1)
         @inbounds x2 = (x1[1], x1[2], x1[3]%L+1)
 
-		r_i = L^3÷4 * (2 + 3(m-1)) + l + 1
-		step(m², ϕ, x1, x2, L, R, r_i)
+		step(m², ϕ, x1, x2, L)
     end
     return
 end
 
 function thermalize(m², ϕ, threads, blocks, N=10000)
-	R = CUDA.randn(N*3L^3)
 	for i in 1:N
-		sweep(m², ϕ, R[range((i-1)*3L^3+1, length=3L^3)], threads, blocks)
+		sweep(m², ϕ, threads, blocks)
 	end
 end
 
@@ -127,13 +123,11 @@ m² = -2.28587
 ϕ .= ϕ .- shuffle(ϕ)
 ϕ = CuArray(ϕ)
 
-N = 1024
+N = 3L^3
 
-R = CUDA.randn(3L^3)
-
-kernel_i = @cuda launch=false gpu_sweep_i(m², ϕ, L, 1, R)
-kernel_j = @cuda launch=false gpu_sweep_j(m², ϕ, L, 1, R)
-kernel_k = @cuda launch=false gpu_sweep_k(m², ϕ, L, 1, R)
+kernel_i = @cuda launch=false gpu_sweep_i(m², ϕ, L, 1)
+kernel_j = @cuda launch=false gpu_sweep_j(m², ϕ, L, 1)
+kernel_k = @cuda launch=false gpu_sweep_k(m², ϕ, L, 1)
 config = launch_configuration(kernel_i.fun)
 threads = min(N, config.threads)
 blocks = cld(N, threads)
