1399b1047e551eef97e3a23bc3de53b1fd9904f5
ottjk <joshott16@gmail.com>
2022-08-04 22:21:22 -0400

    script for reducing volume of data

---
+++ data_trimmer.jl
@@ -0,0 +1,18 @@
+cd("/share/tmschaef/jkott/modelB/KZ/cumulants")
+
+using DelimitedFiles
+using Printf
+
+id = parse(Int, ARGS[1])
+max_run = 832
+
+Threads.@threads for series in 1:16
+    for run in 1:max_run
+    df = readdlm("sum_L_32_id_$(id)_series_$(series)_run_$run.dat", ' ')
+
+    open("trim/sum_L_32_id_$(id)_series_$(series)_run_$run.dat","w") do io
+        for row in 1:10:size(df, 1)
+            Printf.@printf(io, "%i %f %f\n", df[row,1], df[row,2], df[row,3])
+        end
+    end
+end
