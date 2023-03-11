1b782b34c8edbe8ca040ef0be8456d60ec6ec301
Josh Ott <joshott16@gmail.com>
2023-03-11 16:16:26 -0500

    updated analysis code

--- KZ_analysis.jl
+++ KZ_analysis.jl
@@ -1,57 +1,48 @@
-cd("/share/tmschaef/jkott/modelB/KZ/dynamics")
+cd("/share/tmschaef/jkott/modelB/KZ/fft/"*ARGS[1])
 
-using Plots
 using DelimitedFiles
+using Statistics
 
 const L = 32
-const z = 3.906e0
-const Δt = 0.04e0
+const id_max = 16
+const series_max = 16
+const run_max = 128
+const id_size = series_max*run_max
+const n_quenches = id_max*id_size
 
-# KZ protocol variables
-const m²c, m²0, m²e = -2.28587, -2.0e0, -4.0e0
-const t_c = 0.5 * 10^-3 * L^z * (1 - m²0/m²c)
-const τ_Q = t_c * m²c / (m²c - m²0)
-const m_a, m_b = m²c/τ_Q, m²0
+C_tot = zeros(Float32, L÷2+1)
+database = zeros(Float32, n_quenches, L÷2+1)
 
-const t_e = (m²e - m_b)/m_a
-##
-
-const t_c_steps = trunc(Int, t_c/Δt)
-
-maxt = trunc(Int, t_e / Δt)+2
-ts = [1//2, 3//4, 1, 5//4, 5//2, maxt//t_c_steps]
-
-C_tot = zeros(Float32, length(ts), L÷2)
-
-function KZ_run(fn)
+function KZ_run(fn, n)
     df = readdlm(fn, ' ')
-    if (length(df[:,1])) != maxt
-        println("YIKES AT $fn")
-        return
-    end
-    ϕ = zeros(ComplexF32, length(ts), L÷2)
-    τ = trunc.(Int, ts*t_c_steps)
 
-    Threads.@threads for k in 1:L÷2
-        ϕ[:,k] .= df[τ,2k] .+ 1.0im .* df[τ,1+2k]
+    ϕ = zeros(ComplexF32, id_size, L÷2+1)
+
+    Threads.@threads for k in 1:L÷2+1
+        ϕ[:,k] = df[:,2k-1].^2 .+ df[:,2k].^2
     end
 
-    for i in 1:length(ts)
-        C_tot[i,:] .+= abs2.(ϕ[i,:])/4096
-    end
+    database[id_size*(n-1)+1:id_size*n,:] .= ϕ
 end
 
 function collect_files()
-    for id in 1:16, series in 1:16, run in 1:16
-	@show (id, series, run)
-        KZ_run("KZ_L_$L"*"_id_$id"*"_series_$series"*"_run_$run.dat")
+    for id in 1:id_max
+        KZ_run("fft_L_$L"*"_id_$id.dat", id)
     end
 end
 
 collect_files()
 
+C_tot = sum(database, dims=1)[1,:]/n_quenches
+Cerr = std(database, dims=1)/sqrt(n_quenches)
+
 output_file = open("data_KZ_$L.jl","w")
 
 write(output_file, "C_32 = ")
 show(output_file, C_tot)
+write(output_file, "\n\n")
+
+write(output_file, "Cerr_32 = ")
+show(output_file, Cerr[1,:])
+
 close(output_file)
--- cumulant_analysis.jl
+++ cumulant_analysis.jl
@@ -1,69 +1,84 @@
-cd("/share/tmschaef/jkott/modelB/KZ/cumulants")
+cd("/share/tmschaef/jkott/modelB/KZ/cumulants/"*ARGS[1])
 
-using Plots
 using DelimitedFiles
 using Statistics
 
 const L = 32
 const z = 3.906e0
 const Δt = 0.04e0
+const threads = Threads.nthreads()
+const factor = parse(Int, ARGS[1])
 
 # KZ protocol variables
 const m²c, m²0, m²e = -2.28587, -2.0e0, -3.0e0
-const t_c = 0.5 * 10^-3 * L^z * (1 - m²0/m²c)
-const τ_Q = t_c * m²c / (m²c - m²0)
-const m_a, m_b = m²c/τ_Q, m²0
+m_a, m_b = begin
+	τ_R = 2 * 10^-3 * L^z
+    τ_Q = factor * τ_R
 
-const t_e = (m²e - m_b)/m_a
+    m²c/τ_Q, m²0
+end
+
+# In units of time
+t_c = (m²c - m_b) / m_a
+t_e = (m²e - m_b) / m_a
+
+# In units of steps
+skip = 100
+maxt = trunc(Int, t_e / Δt) + 1
+maxt = div(maxt, skip) + 1
 ##
-
-const t_c_steps = trunc(Int, t_c/Δt)
-
-skip = 10
-maxt = (trunc(Int, t_e / Δt)+1)÷skip+2
 
 id_max = 16
 series_max = 16
-run_max = 32
+run_max = 128
 n_files = id_max * series_max * run_max
 
-M4 = zeros(Float64, div(maxt,10)+1, n_files)
-M2 = zeros(Float64, div(maxt,10)+1, n_files)
+M2 = zeros(Float64, maxt, threads)
+M2_err = zeros(Float64, maxt, threads)
 
-function cumulant_run(fn, i)
+function cumulant_run(fn)
     df = readdlm(fn, ' ')
-    M = df[1:10:end,3]
+    M = df[:,3]
 
-    M4[:,i] .= (M*(L^3/2)^(1/4)).^4
-    M2[:,i] .= (M*(L^3/2)^(1/4)).^2
+    M2[:,Threads.threadid()] .+= M.^2 / n_files
+end
+
+function cumulant_err(fn)
+    df = readdlm(fn, ' ')
+    M = df[:,3]
+
+    M2_err[:,Threads.threadid()] .+= (M.^2 .- M2).^2 / n_files
 end
 
 function collect_files()
     Threads.@threads for id in 1:id_max
         for series in 1:series_max, run in 1:run_max
-            cumulant_run("sum_L_$L"*"_id_$id"*"_series_$series"*"_run_$run.dat", run + (series-1)*run_max + (id-1)*series_max*run_max)
+            cumulant_run("sum_L_$L"*"_id_$id"*"_series_$series"*"_run_$run.dat")
         end
     end
+
+    global M2 = sum(M2, dims=2)
+
+    Threads.@threads for id in 1:id_max
+        for series in 1:series_max, run in 1:run_max
+            cumulant_err("sum_L_$L"*"_id_$id"*"_series_$series"*"_run_$run.dat")
+        end
+    end
+
+    global M2_err = sum(M2_err, dims=2)
 end
 
 collect_files()
 
-M4_err = std(M4, dims=2) / sqrt(n_files)
-M2_err = std(M2, dims=2) / sqrt(n_files)
-
-M4 .= sum(M4, dims=2)
-M2 .= sum(M2, dims=2)
-
-c4 = M4 .- 3 * M2.^2
-c4_err = sqrt.(M4_err.^2 .+ (6 * M2 .* M2_err).^2)
+M2_err .= sqrt.(M2_err / n_files)
 
 output_file = open("data_cumulant_$L.jl","w")
 
-write(output_file, "c4 = ")
-show(output_file, c4)
+write(output_file, "c2 = ")
+show(output_file, M2)
 write(output_file, "\n\n")
 
-write(output_file, "c4_err = ")
-show(output_file, c4_err)
+write(output_file, "c2_err = ")
+show(output_file, M2_err)
 
 close(output_file)
